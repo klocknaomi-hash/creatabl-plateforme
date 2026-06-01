439f087cd1a5ba4237c277419aa5eb122f008f98
Naomi Klock <naomi@creatabl.com>
2026-06-01 15:58:35 +0200

    fix: add explicit head link tags for favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,12 @@ export default function RootLayout({
         suppressHydrationWarning
         className={`${outfit.className} ${playfair.variable} h-full antialiased`}
       >
+        <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+          <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        </head>
         <body className="min-h-full flex flex-col">
           <ThemeProvider
             attribute="class"
